27a0723d086e5c1d80371d0f482e2203bfbc7625
Juman <juman.developer@gmail.com>
2023-10-05 09:48:42 +0300

    Initial Commit

---
+++ firstXcode/firstXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstXcodeApp.swift
+//  firstXcode
+//
+//  Created by Juman Dhaher on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct firstXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
