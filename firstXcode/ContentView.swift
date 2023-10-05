50793fc9fa65922b59ec7602c30c511ebef43756
Juman <juman.developer@gmail.com>
2023-10-05 15:32:13 +0300

    homeSummary

--- firstXcode/ContentView.swift
+++ firstXcode/ContentView.swift
@@ -13,14 +13,22 @@ struct ContentView: View {
 
     var body: some View {
         NavigationStack{
-            VStack(alignment: .leading,spacing: 12){
+            ScrollView {  VStack(alignment: .leading,spacing: 12){
                 Text("Summary").font(.largeTitle)
- 
-                boxContent(title:"Absence Days",dec:"Exceeding 15 days of absences without an excuse will disqualify you from the program.",precentage: "5/15")
-              
-                boxContent(title:"Absence Days",dec:"Exceeding 15 days of absences without an excuse will disqualify you from the program.",precentage: "5/15")
+                boxContent(title:"Absence Days",dec:"Exceeding 15 days of absences without an excuse will disqualify you from the program.",precentageText: "5/15",precentage:0.5,firstColor:  Color.pink.opacity(0.5),secondColor: Color.pink)
                 
-                boxContent(title:"Absence Days",dec:"Exceeding 15 days of absences without an excuse will disqualify you from the program.",precentage: "5/15")
+                boxContent(title:"Late Hours",dec:"Arriving late for a total of 4 hours will be counted as an unexcused absence.",precentageText: "1/4",precentage:0.8,firstColor:  Color.cyan.opacity(0.5),secondColor: Color.cyan)
+                
+                boxContent(title:"Monthly Leave",dec:"You are allowed 2 hours of monthly leave each month.",precentageText: "0/2",precentage:100,firstColor:  Color.purple.opacity(0.5),secondColor: Color.purple)
+                
+                Text("The Guide book").font(.title)
+                
+                ZStack(alignment:.trailing){
+                    RoundedRectangle(cornerRadius: 25).fill(Color.white)
+                    Text("• يجب على المتدربة تسجيل الدخول لاحتساب الحضور بمجرد الدخول لمبنى الأكاديمية، وعند التأخر ١٥ دقيقة في تسجيل الدخول سيتم تلقائياً احتساب تأخير المتدربات.").lineLimit(4).padding()
+                }.background(Color.white)
+                    .cornerRadius(12)
+                    .frame(width: .infinity,height: 120)
                 
                 Spacer()
                 
@@ -29,44 +37,44 @@ struct ContentView: View {
             .padding(.all)
             .background(Color(#colorLiteral(red: 0.9647058845, green: 0.9647058845, blue: 0.9647058845, alpha: 1) )).ignoresSafeArea()
             .navigationTitle("Juman Dhaher").padding(.top)
-            /*.navigationBarItems(leading:
-                VStack(alignment: .leading) {
-                   Text("Juman Dhaher").font(.headline)
-                Text("jdhaher@twq.idserve.net").font(.subheadline)
-            }
-             )*/
+                /*.navigationBarItems(leading:
+                 VStack(alignment: .leading) {
+                 Text("Juman Dhaher").font(.headline)
+                 Text("jdhaher@twq.idserve.net").font(.subheadline)
+                 }
+                 )*/
             .toolbar{
-              
+                
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Text("Sign out")
                 }
             }
-            
-        }
+                
+            }}
     }
     
-    func boxContent(title: String,dec:String,precentage:String) -> some View  {
+    func boxContent(title: String,dec:String,precentageText:String,precentage:Double,firstColor:Color,secondColor:Color) -> some View  {
        return ZStack (alignment: .topLeading) {
             RoundedRectangle(cornerRadius: 25).fill(Color.white)
             HStack{
                 VStack(alignment:.leading){
                     Text(title).font(.title2)
-                    Text(dec).font(Font.system(size: 12)).frame(maxWidth:140)
+                    Text(dec).font(Font.system(size: 12)).frame(maxWidth:140).foregroundColor(.gray)
                 }
                 Spacer()
                 ZStack {
                            Circle()
                                .stroke(
-                                   Color.red.opacity(0.5),
+                                   firstColor,
                                    lineWidth: 15
                                )
-                    Text(precentage).font(.title3)
+                    Text(precentageText).font(.title3)
                            Circle()
-                        .trim(from: 0, to: 0.8) // 1
+                        .trim(from: 0, to: precentage) // 1
                                .stroke(
-                                   Color.red,
+                                   secondColor,
                                    lineWidth: 15
-                               )
+                               ).rotationEffect(.degrees(-90))
                        }
                         
             }.padding()
