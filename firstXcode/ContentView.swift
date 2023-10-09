b42e6dc63a06d3b3b9ca8e2a2ee99b53d8c86734
Juman <juman.developer@gmail.com>
2023-10-09 11:04:40 +0300

    add colors for light mode and dark mode

--- firstXcode/ContentView.swift
+++ firstXcode/ContentView.swift
@@ -17,20 +17,27 @@ struct ContentView: View {
                 Text("Summary").font(.largeTitle)
                 
                 NavigationLink(destination:AbsenceDaysView(), label:{
-                    boxContent(title:"Absence Days",dec:"Exceeding 15 days of absences without an excuse will disqualify you from the program.",precentageText: "5/15",precentage:0.5,firstColor:  Color.pink.opacity(0.5),secondColor: Color.pink)
+                    boxContent(title:"Absence Days",dec:"Exceeding 15 days of absences without an excuse will disqualify you from the program.",precentageText: "5/15",precentage:0.5,
+                               firstColor: Color("PinkColor").opacity(0.5) ,secondColor:Color("PinkColor"))
                 })
                 NavigationLink(destination:LateHoursView(), label:{
-                    boxContent(title:"Late Hours",dec:"Arriving late for a total of 4 hours will be counted as an unexcused absence.",precentageText: "1/4",precentage:0.8,firstColor:  Color.cyan.opacity(0.5),secondColor: Color.cyan)
+                    boxContent(title:"Late Hours",dec:"Arriving late for a total of 4 hours will be counted as an unexcused absence.",precentageText: "1/4",precentage:0.8,
+                               firstColor:Color("GreenColor").opacity(0.5),
+                               secondColor:Color("GreenColor"))
                 })
-                boxContent(title:"Monthly Leave",dec:"You are allowed 2 hours of monthly leave each month.",precentageText: "0/2",precentage:100,firstColor:  Color.purple.opacity(0.5),secondColor: Color.purple)
+                    boxContent(title:"Monthly Leave",dec:"You are allowed 2 hours of monthly leave each month.",precentageText: "0/2",precentage:0.5,
+                               firstColor:Color("PurpleColor").opacity(0.5),
+                        secondColor:Color("PurpleColor")
+                )
                 
                 Text("The Guide book").font(.title)
                 
                 NavigationLink(destination: GuidBookView(), label:{
-                    ZStack(alignment:.trailing){
-                        RoundedRectangle(cornerRadius: 25).fill(Color.white)
-                        Text("• يجب على المتدربة تسجيل الدخول لاحتساب الحضور بمجرد الدخول لمبنى الأكاديمية، وعند التأخر ١٥ دقيقة في تسجيل الدخول سيتم تلقائياً احتساب تأخير المتدربات.").multilineTextAlignment(.trailing).lineLimit(4).padding().foregroundColor(.black)
-                    }.background(Color.white)
+                    ZStack(alignment:.center){
+                        RoundedRectangle(cornerRadius: 25).fill(Color("GreenColor"))
+                        Image(systemName: "book.circle.fill").resizable().frame(width: 80, height: 80).foregroundColor(Color("CardColor"))
+
+                    }.background(Color("GreenColor"))
                         .cornerRadius(12)
                         .frame(height: 120)
                 })
@@ -50,7 +57,7 @@ struct ContentView: View {
                 
             }
             .padding(.all)
-            .background(Color(#colorLiteral(red: 0.9647058845, green: 0.9647058845, blue: 0.9647058845, alpha: 1) )).ignoresSafeArea()
+            .background(  Color("Background")).ignoresSafeArea(.all)
             .navigationTitle("Juman Dhaher")
             .padding(.top)
             .toolbar{
@@ -58,23 +65,22 @@ struct ContentView: View {
                     Text("Sign out")
                 })
             }
-        }
-            
-   } .navigationBarBackButtonHidden(true)
+            }
+        } .navigationBarBackButtonHidden(true)
     }
     
     func boxContent(title: String,dec:String,precentageText:String,precentage:Double,firstColor:Color,secondColor:Color) -> some View  {
        return ZStack (alignment: .topLeading) {
-            RoundedRectangle(cornerRadius: 25).fill(Color.white)
+            RoundedRectangle(cornerRadius: 25).fill(Color("CardColor"))
             HStack{
                 VStack(alignment:.leading){
                     Text(title)
                         .font(.title2)
-                        .foregroundColor(.black)
+                        .foregroundColor(Color("TitleColor"))
                     Text(dec)
                         .font(Font.system(size: 12))
                         .frame(maxWidth:140)
-                        .foregroundColor(.gray)
+                        .foregroundColor(Color("SubtitleColor"))
                         .multilineTextAlignment(.leading)
                 }
                 Spacer()
@@ -86,7 +92,7 @@ struct ContentView: View {
                     )
                     Text(precentageText)
                         .font(.title3)
-                        .foregroundColor(.black)
+                        .foregroundColor(Color("TitleColor"))
                     Circle()
                         .trim(from: 0, to: precentage) // 1
                         .stroke(
@@ -96,7 +102,7 @@ struct ContentView: View {
                     }
             }.padding()
 
-        } .background(Color.white)
+        } .background(Color("CardColor"))
             .cornerRadius(12)
             .frame(height: 130)
     }
